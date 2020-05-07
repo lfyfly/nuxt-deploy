61838e745bd8d4933cf79930835fa4778798ac31
lfyfly <410793635@qq.com>
2020-05-07 13:04:28 +0800

    test update

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
           target="_blank"
           class="button--grey"
         >
-          GitHub hello
+          GitHub lfy
         </a>
         <ul>
           <li>NODE_ENV:{{ NODE_ENV }}</li>
